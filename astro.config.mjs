5f97e170f2c6567fedb0debf497c8ffd9601dc60
arian81 <ahmadinejadarian@gmail.com>
2022-11-07 17:19:10 -0500

    bypass adblocks for analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image(), partytown()]
 });
