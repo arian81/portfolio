542e6f3f3df5c1bfbb83c0eca10c2e8077162dfb
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2023-02-02 15:01:22 -0500

    migrate to ass 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,12 @@ import image from "@astrojs/image";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+
+// https://astro.build/config
+
+// https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image(), react()]
 });
