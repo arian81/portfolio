cc6424ea474a711bbee79be81baa3efc13452b16
fvcci <fongf2@mcmaster.ca>
2023-04-03 19:11:12 -0400

    fixed footer overlap issue and added searching on enter key

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -2,7 +2,7 @@ const Copyright = () => {
   let start = new Date();
   let year = start.getFullYear().toString();
   return (
-    <div className="absolute bottom-0 p-2">
+    <div className="p-2">
       <div className="flex flex-col items-center justify-center">
         <p className="text-sm text-[#592406] dark:text-[#ccc] dark:opacity-80">
           Made with ♥ in <a href="https://astro.build">Astro.js</a>
