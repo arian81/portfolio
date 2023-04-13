8497173b02f06892774dc0e1527aeee85199c24a
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2023-04-13 19:56:31 -0400

    remove commented code

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -18,40 +18,6 @@ export default function Custom404() {
     animationData: dark404data,
   };
   return (
-    //     <div class="flex flex-col items-center justify-center w-full h-full">
-    //   <div class="dark:hidden">
-    //     {/* <lottie-player
-    //       src="/404_light.json"
-    //       background="transparent"
-    //       speed="0.3"
-    //       style="width: full; height: full "
-    //       loop
-    //       autoplay></lottie-player> */}
-    //       <Lottie options={light404} height={400} width={400} />
-    //   </div>
-    //   <div class="hidden dark:block">
-    //     {/* <lottie-player
-    //       src="/404_dark.json"
-    //       background="transparent"
-    //       speed="0.3"
-    //       style="width: full; height: full "
-    //       loop
-    //       autoplay></lottie-player> */}
-    //     <Lottie options={dark404} height={400} width={400} />
-    //   </div>
-    //   <div>
-    //     <p
-    //       class="text-[#592406] text-xl md:text-4xl font-bold dark:text-white opacity-80"
-    //     >
-    //       This page doesn't exist (yet?)
-    //     </p>
-    //     <div class="flex items-center justify-center p-5">
-    //       <div
-    //         class="w-10 md:w-16 text- dark:text-orange-200 dark:opacity-80 text-[#592406]"
-    //       >
-    //         <a href="/"><HomeIcon /></a>
-    //       </div>
-    // <div/>
     <main className="relative min-h-screen w-full bg-orange-200 dark:bg-[#161616]">
       <div className="m-auto flex min-h-screen w-auto flex-col items-center justify-center gap-10 pb-24 pt-48">
         <div className="absolute top-0 w-full p-2">
