27eb4353a50cd2e86e76a7aa94243e4f37e907d5
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2024-01-17 01:03:27 -0500

    try without ssg

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  output: "export",
+  // output: "export",
 
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
