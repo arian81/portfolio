b8059109a53c5d93208112b0ead151dbdd5d851d
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2024-01-17 00:46:21 -0500

    feat: setup sanity

---
+++ sanity/schemas/post.ts
@@ -0,0 +1,71 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "post",
+  title: "Post",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    }),
+    defineField({
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "reference", to: { type: "category" } }],
+    }),
+    defineField({
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+    }),
+    defineField({
+      name: "summary",
+      title: "Summary",
+      type: "string",
+    }),
+    defineField({
+      name: "body",
+      title: "Body",
+      type: "blockContent",
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      media: "mainImage",
+      summary: "summary",
+    },
+    prepare(selection) {
+      const { summary } = selection;
+      return { ...selection, subtitle: summary };
+    },
+  },
+});
