0452ca93ca9591aa3735d06ad39020aa048169b5
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2024-06-05 23:22:28 -0400

    fix: typescript errors

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -4,8 +4,7 @@ import blockContent from "./schemas/blockContent";
 import category from "./schemas/category";
 import post from "./schemas/post";
 import project from "./schemas/project";
-import technology from "./schemas/Technology";
-// import author from './schemas/author'
+import technology from "./schemas/technology";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [post, category, project, blockContent, technology],
