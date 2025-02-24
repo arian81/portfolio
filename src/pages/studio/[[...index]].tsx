ce3b6c13c8bf93a7e3aa9d4aef888fc06f39d071
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2025-02-23 19:45:41 -0500

    feat: update node pacakges

--- src/pages/studio/[[...index]].tsx
+++ src/pages/studio/[[...index]].tsx
@@ -1,15 +1,13 @@
 import Head from 'next/head'
 import { NextStudio } from 'next-sanity/studio'
-import { metadata } from 'next-sanity/studio/metadata'
 import config from '../../../sanity.config'
 
 export default function StudioPage() {
   return (
     <>
       <Head>
-        {Object.entries(metadata).map(([key, value]) => (
-          <meta key={key} name={key} content={value} />
-        ))}
+        <meta name="robots" content="noindex" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
       </Head>
       <NextStudio config={config} />
     </>
