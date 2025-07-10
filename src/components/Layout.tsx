3a3d2534bd02abec2d0f289840e63a7a9bff3d19
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2025-07-10 00:20:44 -0400

    chore: remove overflow auto
    Having this will make finding bugs harder

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex min-h-screen w-full flex-col overflow-auto bg-orange-200 dark:bg-[#161616]">
+    <div className="flex min-h-screen w-full flex-col bg-orange-200 dark:bg-[#161616]">
       <Header />
       <main className="flex flex-1">{children}</main>
       <Footer />
