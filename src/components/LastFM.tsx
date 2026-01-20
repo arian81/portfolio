1cec0cc416f525491f5f21df783c1beee2bcb41f
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2026-01-20 17:12:25 -0500

    fix: add lastfm to trusted image cdn

--- src/components/LastFM.tsx
+++ src/components/LastFM.tsx
@@ -21,6 +21,9 @@ const CurrentlyPlaying = () => {
     return <div className="-translate-y-[35%] opacity-0" aria-hidden="true" />;
   }
 
+
+  console.log(JSON.stringify(lastFM, null, 2));
+
   return (
     <a
       href={lastFM.song.url}
