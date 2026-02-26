52e69b999f977787b8ed4e82269bac1c482f2b66
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2026-02-25 19:59:14 -0500

    fix: improve item spacing

--- src/lib/components/Item.svelte
+++ src/lib/components/Item.svelte
@@ -9,10 +9,9 @@
 	let { href, title, description, date }: Props = $props();
 </script>
 
-<a class="flex items-end" {href}>
+<a class="flex items-end gap-3" {href}>
 	<span>{title}</span>
 	<span class="text-sm text-gray-500">{description}</span>
-
-	<span class="flex-1 grow -translate-y-1.5 border-b border-dotted border-gray-500"></span>
-	<span class="">{date}</span>
+	<span class="flex-1 grow -translate-y-2.5 border-b border-dotted border-gray-500"></span>
+	<span class="text-sm text-gray-500">{date}</span>
 </a>
