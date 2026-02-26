fa5feebc5628fcabeae2065572780baf760c53a0
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2026-02-26 17:35:40 -0500

    fix: consistent coloring

--- src/lib/components/projectItem.svelte
+++ src/lib/components/projectItem.svelte
@@ -7,7 +7,7 @@
 	<div class="bg-blue-100">
 		<enhanced:img src={image} alt={title} class="p-10" />
 		<h3 class="text-lg font-bold">{title}</h3>
-		<p class="text-sm text-gray-500">{description}</p>
-		<p class="text-sm text-gray-500">{repo}</p>
+		<p class="text-sm text-stone-500">{description}</p>
+		<p class="text-sm text-stone-500">{repo}</p>
 	</div>
 </a>
