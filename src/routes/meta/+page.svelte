3042fde9aacbc3e99a8c39683f5dc8e05be92ca5
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2026-03-06 20:49:04 -0500

    feat: add logo, sitemap, og

---
+++ src/routes/meta/+page.svelte
@@ -0,0 +1,17 @@
+<div class="flex h-full w-full items-center justify-center gap-16 bg-yellow-50 px-20">
+	<div class="flex flex-col">
+		<h1
+			class="font-instrument text-[100px] leading-[0.9] font-medium tracking-wider text-stone-800"
+		>
+			Arian<br />Ahmadinejad
+		</h1>
+		<span class="mt-4 text-sm tracking-[0.3em] text-stone-400">arian.gg</span>
+	</div>
+	<div class="h-40 w-px bg-linear-to-b from-transparent via-stone-300 to-transparent"></div>
+	<p class="max-w-95 text-xl leading-relaxed text-stone-500">
+		I like to build <u class="text-stone-700 decoration-stone-300">intentional</u> software with
+		utmost
+		<strong class="font-semibold text-stone-700">simplicity</strong>
+		that <i class="text-stone-700">just works</i>.
+	</p>
+</div>
